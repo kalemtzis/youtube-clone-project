75b618067264abada5ea5376431d2f281962223b
kalemtzis <kalemtzis@csd.auth.gr>
2025-08-17 12:50:48 +0300

    sync database with clerk throw webhook

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher([
-  '/sign-in(.*)',
-  '/sign-up(.*)',
-  '/'
-])
+const isPrivateRoute = createRouteMatcher([
+  
+]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) await auth.protect()
+  if (isPrivateRoute(req)) await auth.protect();
 });
 
 export const config = {
