bceb338df6b74b8730f603fd3135bcd1043ccc10
kalemtzis <kalemtzis@csd.auth.gr>
2025-08-23 18:20:47 +0300

    add extra info to studio page for videos

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -172,9 +172,9 @@ const VideosSectionSuspense = () => {
                   <TableCell className="text-sm truncate">
                     {format(new Date(video.createdAt), "d MMM yyyy")}
                   </TableCell>
-                  <TableCell className="text-right text-sm">views</TableCell>
-                  <TableCell className="text-right text-sm">comments</TableCell>
-                  <TableCell className="text-right text-sm pr-6">likes</TableCell>
+                  <TableCell className="text-right text-sm">{video.viewCount}</TableCell>
+                  <TableCell className="text-right text-sm">{video.commentCount}</TableCell>
+                  <TableCell className="text-right text-sm pr-6">{video.likeCount}</TableCell>
                 </TableRow>
               ))}
           </TableBody>
